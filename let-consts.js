c686938df7bd736f6b3a80f41493eea99444f42e
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-19 23:17:13 +0600

    git push

--- let-consts.js
+++ let-consts.js
@@ -8,3 +8,10 @@ console.log(countryName);
 let price = 120;
 price = 150;
 console.log(price)
+
+const student = {
+    name: 'Jeya',
+     age:  19
+}
+
+console.log(student) // মনে রাখতে হবে যে const সহ একটা অবজেক্ট ২ বার ব্যবহার করা যায় না।
